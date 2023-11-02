813231d5178c21f6c9c9f3714009cdf3a2a46efc
Achim Lenz <hello.achim@outlook.com>
2023-11-02 11:41:36 +0100

    Add Class and Method and first Test

---
+++ src/test/java/org/example/PlayerCharacterTest.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class PlayerCharacterTest {
+
+    @Test
+    void getX_whenStarted_thenReturnZero(){
+        // GIVEN
+        int expected = 0;
+        // WHEN
+        int actual = PlayerCharacter.getX();
+        // THEN
+        assertEquals(expected, actual);
+
+    }
+
+}
